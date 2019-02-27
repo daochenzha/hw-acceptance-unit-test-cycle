bb200e7eeac17e4cc55675df24c2fb1472e1c5e2
EC2 Default User <ec2-user@ip-172-31-9-4.us-east-2.compute.internal>
2019-02-27 18:36:29 +0000

    Part seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
-    	  {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'},
-    	  {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
-      	  {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'},
-      	  {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001'},
-      	  {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001'},
-      	  {:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968'},
-      	  {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004'},
-      	  {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'},
-      	  {:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000'},
+movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992', :director => 'test'},
+    	  {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984', :director => 'test'},
+    	  {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989', :director => 'test'},
+      	  {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011', :director => 'test'},
+      	  {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001', :director => 'test'},
+      	  {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001', :director => 'test'},
+      	  {:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968', :director => 'test'},
+      	  {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004', :director => 'test'},
+      	  {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981', :director => 'test'},
+      	  {:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000', :director => 'test'},
   	 ]
 
 movies.each do |movie|
